3124aad880a9988dfe2ec2cda9321e5027fdaa95
simba1949 <simba1949@outlook.com>
2023-08-17 21:27:27 +0800

    style：包重命名

--- netty-base/src/main/java/top/simba1949/websocket/LongConnectionServer.java
+++ netty-base/src/main/java/top/simba1949/websocket/LongConnectionServer.java
@@ -1,4 +1,4 @@
-package top.simba1949.longConnection;
+package top.simba1949.websocket;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
--- netty-base/src/main/java/top/simba1949/websocket/LongConnectionServerHandler.java
+++ netty-base/src/main/java/top/simba1949/websocket/LongConnectionServerHandler.java
@@ -1,4 +1,4 @@
-package top.simba1949.longConnection;
+package top.simba1949.websocket;
 
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
